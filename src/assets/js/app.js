c1bc4426729a463215249704f6146e227dd90689
Daeng Rosanda <daengrosanda@gmail.com>
2020-01-05 17:34:35 +0700

    Update all assets.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,29 @@
 import $ from 'jquery';
 
+var is_log = 0;
 window.$ = $;
 window.jQuery = $;
+
+import NProgress from "nprogress";
+window.NProgress = NProgress;
+
+require("bootstrap-sass");
+require("select2");
+require("jquery-validation");
+require("jquery-sparkline");
+require("jquery.flot");
+require("jquery.resize");
+require("jquery-slimscroll");
+
+if(is_log){
+console.log("jquery loaded ?", $ instanceof Function);
+console.log("jquery.validate loaded?", $().validate instanceof Function);
+console.log("jquery.sparkline loaded?", $().sparkline instanceof Function);
+console.log("jquery.flot loaded?", $().flot instanceof Function);
+console.log("jquery.plot loaded?", $().plot instanceof Function);
+console.log("jquery.resize loaded?", $().resize instanceof Function);
+console.log("jquery.slimScroll loaded?", $().slimScroll instanceof Function);
+console.log("select2 loaded?", $().select2 instanceof Function);
+console.log("NProgress is a", typeof NProgress );
+console.log("NProgress loaded?", NProgress.start instanceof Function);
+}
